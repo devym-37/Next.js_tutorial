d6735e7a5ecb5072b1790e9c756ede2828532570
LeeYoungMin <miniyoung37@gmail.com>
2020-05-20 23:24:27 +0900

    step6_동적 routing

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,24 @@ import Link from "next/link";
 
 const PostLink = (props) => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
+    <Link href="/p/[id]" as={`/p/${props.id}`}>
+      <a>{props.id}</a>
     </Link>
   </li>
 );
+
 export default function Blog() {
   return (
     <Layout>
       <h1>My Blog</h1>
       <ul>
-        <PostLink title="Hello Next.js" />
-        <PostLink title="Learn Next.js is awesome" />
-        <PostLink title="Deploy apps with Zeit" />
+        <PostLink id="hello-nextjs" />
+        <PostLink id="learn-nextjs" />
+        <PostLink id="deploy-nextjs" />
       </ul>
     </Layout>
   );
 }
 
 // <Link href="/about" title="About page"> 효과가 없음. link 컴포넌트는 href와 같은 일부 props만 받는 HOC(Higher Order Component)이기 때문이다.
+// as={ } 브라우저 URL에 보여질 URL
