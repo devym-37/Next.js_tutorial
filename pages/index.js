7a8e6a44b6f66e6927ed8903b5198e0a6bb64ff3
LeeYoungMin <miniyoung37@gmail.com>
2020-05-20 23:15:09 +0900

    step5_query string 동적 페이지 useRouter()

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import Layout from "../components/Layout";
+import Link from "next/link";
 
-export default function Index() {
+const PostLink = (props) => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+export default function Blog() {
   return (
     <Layout>
-      <p>Hello Next.js</p>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
     </Layout>
   );
 }
+
 // <Link href="/about" title="About page"> 효과가 없음. link 컴포넌트는 href와 같은 일부 props만 받는 HOC(Higher Order Component)이기 때문이다.
