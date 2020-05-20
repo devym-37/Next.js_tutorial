c2a6c2e1951259937c4e7c0070a97e6aa5ffd270
LeeYoungMin <miniyoung37@gmail.com>
2020-05-20 22:38:30 +0900

    step4_Using Shared Components Layout

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-import Header from "../components/Header";
+import Layout from "../components/Layout";
 
-const Index = () => (
-  <div>
-    <Header />
-    <p>Hello Next.js</p>
-  </div>
-);
-
-export default Index;
-
+export default function Index() {
+  return (
+    <Layout>
+      <p>Hello Next.js</p>
+    </Layout>
+  );
+}
 // <Link href="/about" title="About page"> 효과가 없음. link 컴포넌트는 href와 같은 일부 props만 받는 HOC(Higher Order Component)이기 때문이다.
