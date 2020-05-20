0e8bbdc6e6f2f99ebe8e3283afd910801279af1f
LeeYoungMin <miniyoung37@gmail.com>
2020-05-20 22:10:53 +0900

    step2_Link component route

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 const Index = () => (
   <div>
+    <Link href="/about">
+      <a>about Page</a>
+    </Link>
     <p>Hello Next.js</p>
   </div>
 );
 
 export default Index;
+
+// <Link href="/about" title="About page"> 효과가 없음. link 컴포넌트는 href와 같은 일부 props만 받는 HOC(Higher Order Component)이기 때문이다.
